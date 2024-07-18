bc6e1fd24dfc45cb3f590e6e6cced23cd30c6cd7
Sílvio Ricardo <silviocastro006@gmail.com>
2024-07-18 10:30:48 -0300

    alteração custom tkinter

---
+++ Udemy_Luiz.Miranda/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+# 1 - Faça um programa que peça ao usuário para digitar um número inteiro, informe se este número é par ou ímpar. Caso o usuário não digite um número inteiro, informe que não é um número inteiro.
